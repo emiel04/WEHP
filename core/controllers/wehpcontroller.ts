30e8e6f68994ca9e8b6854393fad71962591303c
Emiel <emiel.delaey@student.howest.be>
2024-06-12 22:43:07 +0200

    added DTO

--- core/controllers/wehpcontroller.ts
+++ core/controllers/wehpcontroller.ts
@@ -1,6 +1,38 @@
+import { BodyParsingError, StateError, WEHPError } from '../errors/WEHPError';
+import { prisma } from '../../prisma';
+import { NextFunction, Request, Response } from "express";
+import { toUserDTO } from '../dtos/dto';
+
+export type TUserCreate = {
+    name: string | undefined
+}
+
 class WEHPController {
-    async all() {
-      
+    createAccount = async (userCreate: TUserCreate) => {
+        const {name} = userCreate;
+        
+        if (!name) {
+            throw new StateError('Name is required')
+        }
+
+        let user = await prisma.user.findFirst(
+            {
+                where: { name } 
+                
+            });
+
+        if(user){
+            throw new BodyParsingError('User already exists')
+        }
+        
+
+        const createdUser = await prisma.user.create({
+            data: {
+                name: name
+            }
+        })
+
+        return toUserDTO(createdUser);
     }
 }
 
--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import * as express from "express";
 import * as path from "path";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./prisma";
+import { wehpController } from "./core/controllers/wehpcontroller";
+import { WEHPError } from "./core/errors/WEHPError";
 
-const prisma = new PrismaClient()
 const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 3000;
 
+app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs');
@@ -14,10 +16,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/api', (req, res) => {
+app.post('/api', (req, res) => {
   res.json({"msg": "Hello world"});
+});
+
+app.get('/api/signup', (req, res, next) => {
+  wehpController.createAccount(req.body)
+  .then((createdUser) => res.json(createdUser))
+  .catch((err) => next(err))
 });
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
+});
+
+app.use((error: any, req: express.Request, res: express.Response, next: express.NextFunction) => {  
+  if (error?.statusCode) {
+    res.status(error.statusCode).json({ error: error.message });
+  } else {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
